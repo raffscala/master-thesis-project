252dabdf74e6f51faeb4842751c23fdf5d88a5e5
Raffaele Scala <155641289+raffscala@users.noreply.github.com>
2024-01-05 11:52:39 +0100

    Update and rename cat_cosmo_bin_after.py to catalogs_cosmorate_binary.py

--- plot/catalogs_cosmorate_binary.py
+++ plot/catalogs_cosmorate_binary.py
@@ -6,9 +6,6 @@ z=['0.1','1.0','2.0','4.0','6.0','10.0']
 
 fig = plt.figure()
 ax=[]
-#ax.append(fig.add_axes([0.05, 0.5, 0.3, 0.4]))
-#ax.append(fig.add_axes([0.35, 0.5, 0.3, 0.4]))
-#ax.append(fig.add_axes([0.65, 0.5, 0.3, 0.4]))
 ax.append(fig.add_axes([0.09, 0.15, 0.3, 0.4]))
 ax.append(fig.add_axes([0.39, 0.15, 0.3, 0.4]))
 ax.append(fig.add_axes([0.69, 0.15, 0.3, 0.4]))
@@ -18,12 +15,13 @@ ax.insert(2,fig.add_axes([0.69, 0.55, 0.3, 0.4]))
 plt.rcParams['font.size']='25'
 
 for i in range(len(z)):
-  
+
+  #CHANGE FOR THE CORRECT FILES
   pisn=pd.read_csv(f"/home/scala/tesi/cosmo_rate_public-v2/astromodel/popI_II/A3/10^6/input_bin_pisn_after/output_cosmo_Rate/bin_PISNafter/bin_PISNafter_bin_pisn_after_MandF2017_z{z[i]}_50_SFR_pw.dat", sep=" ")
 
   ppisn=pd.read_csv(f"/home/scala/tesi/cosmo_rate_public-v2/astromodel/popI_II/A3/10^6/input_bin_ppisn_after/output_cosmo_Rate/bin_PPISNafter/bin_PPISNafter_bin_ppisn_after_MandF2017_z{z[i]}_50_SFR_pw.dat", sep=" ")
   
-  
+  #Construction of the subplots
   ax[i].hist(pisn['M1[Msun]'],bins=50,log=True,density=True,histtype='step',color='red',label='PISN after merger')
   ax[i].hist(ppisn['M1[Msun]'],bins=75,log=True,density=True,histtype='step',color='blue',label='PPISN after merger')
   ax[i].text(0.15,0.9,f'z={z[i]}',va='center',ha='center',transform=ax[i].transAxes)
