a11619cc204a99d0beb11efd7274aecb50fe0821
Raffaele Scala <155641289+raffscala@users.noreply.github.com>
2024-01-05 11:49:08 +0100

    Update and rename cat_cosmo_sing.py to catalogs_cosmorate.py

--- plot/catalogs_cosmorate.py
+++ plot/catalogs_cosmorate.py
@@ -16,12 +16,13 @@ ax.insert(2,fig.add_axes([0.69, 0.55, 0.3, 0.4]))
 plt.rcParams['font.size']='25'
 
 for i in range(len(z)):
-  
+
+  #CHANGE FOR THE CORRECT FILES FOR PISN AND PPISN
   pisn=pd.read_csv(f"/home/scala/tesi/cosmo_rate_public-v2/astromodel/popI_II/A3/10^6/input_sing_pisn/output_cosmo_Rate/sing_PISN/sing_PISN_sing_pisn_MandF2017_z{z[i]}_50_SFR_pw.dat", sep=" ")
 
   ppisn=pd.read_csv(f"/home/scala/tesi/cosmo_rate_public-v2/astromodel/popI_II/A3/10^6/input_sing_ppisn/output_cosmo_Rate/sing_PPISN/sing_PPISN_sing_ppisn_MandF2017_z{z[i]}_50_SFR_pw.dat", sep=" ")
-  
-  
+
+  #CONSTRUCTION OF THE SUBPLOTS
   ax[i].hist(pisn['Mzams[Msun]'],bins=50,log=True,density=True,histtype='step',color='red',label='PISN')
   ax[i].hist(ppisn['Mzams[Msun]'],bins=75,log=True,density=True,histtype='step',color='blue',label='PPISN')
   ax[i].text(0.15,0.9,f'z={z[i]}',va='center',ha='center',transform=ax[i].transAxes)
